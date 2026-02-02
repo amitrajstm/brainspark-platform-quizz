1a5f5d3ecdedd9a33182bc5503eb3024c6193500
Amit Kumar Raj <amitraj70913@gmail.com>
2026-02-02 08:14:16 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
         <img
           src="/logo.png"
           alt="BrainSpark Logo"
-          className="w-[96px] h-[80px] object-contain"
+          className="w-[111px] h-[80px] object-contain"
 
         />
       </Link>
