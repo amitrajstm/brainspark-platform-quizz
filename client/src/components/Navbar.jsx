6d86211de98afd31244957eb73d8c28490787c8e
Amit Kumar Raj <amitraj70913@gmail.com>
2026-02-02 07:58:50 +0530

    Update Navbar logo height

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
         <img
           src="/logo.png"
           alt="BrainSpark Logo"
-          className="w-20 h-25 object-contain"
+          className="w-20 h-31 object-contain"
         />
       </Link>
 
